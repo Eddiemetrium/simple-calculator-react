42286cec384a86fbf90c4214e74c340909d3e2c1
Eddiemetrium <eddiemitch262@gmail.com>
2022-10-22 18:42:40 +0300

    React JS Calculator

---
+++ src/OperationDigitButton.js
@@ -0,0 +1,8 @@
+import { ACTIONS } from "./App";
+export default function OperationDigitButton ({dispatch,operation}){
+return (
+  <button
+    onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>{operation}
+  </button>
+);
+}
